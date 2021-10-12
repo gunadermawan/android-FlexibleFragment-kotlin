4cda2f969008eca6c426528d802457cf1b2249d3
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-10-12 10:44:06 +0700

    merubah sintaks fragmentManager dengan android ktx

--- app/src/main/java/com/gunder/myflexiblefragment/HomeFragment.kt
+++ app/src/main/java/com/gunder/myflexiblefragment/HomeFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.commit
+
 class HomeFragment : Fragment(), View.OnClickListener {
 
     override fun onCreateView(
@@ -25,10 +27,13 @@ class HomeFragment : Fragment(), View.OnClickListener {
     override fun onClick(v: View?) {
         val mCategoryFragment = CategoryFragment()
         val mFragmentManager = parentFragmentManager
-        mFragmentManager.beginTransaction().apply {
-            replace(R.id.frame_container, mCategoryFragment, CategoryFragment::class.java.simpleName)
+        mFragmentManager.commit {
             addToBackStack(null)
-            commit()
+            replace(
+                R.id.frame_container,
+                mCategoryFragment,
+                CategoryFragment::class.java.simpleName
+            )
         }
     }
 }
--- app/src/main/java/com/gunder/myflexiblefragment/MainActivity.kt
+++ app/src/main/java/com/gunder/myflexiblefragment/MainActivity.kt
@@ -3,6 +3,7 @@ package com.gunder.myflexiblefragment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.commit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,12 +16,9 @@ class MainActivity : AppCompatActivity() {
 
         if (fragment !is HomeFragment) {
             Log.d("MyFlexibleFragment", "Fragment name :" + HomeFragment::class.java.simpleName)
-            mFragmentManager
-//                    .beginTransaction() untuk memulai proses perubahan.
-                .beginTransaction()
-//                    Metode add() akan menambahkan objek fragment ke dalam layout container
-                .add(R.id.frame_container, mHomeFragment, HomeFragment::class.java.simpleName)
-                .commit()
+            mFragmentManager.commit {
+                add(R.id.frame_container, mHomeFragment, HomeFragment::class.java.simpleName)
+            }
         }
 
     }
